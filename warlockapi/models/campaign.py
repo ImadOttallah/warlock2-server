da863b6f9336476c845be2f8422d5a4f83de1aa0
Imad Ottallah <imadotta@gmail.com>
2023-01-30 18:37:28 -0600

    test

--- warlockapi/models/campaign.py
+++ warlockapi/models/campaign.py
@@ -7,9 +7,9 @@ from .cast import Cast
 class Campaign(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    character = models.ForeignKey(Character, on_delete=models.CASCADE)
-    npc = models.ForeignKey(Npc, on_delete=models.CASCADE)
-    cast = models.ForeignKey(Cast, on_delete=models.CASCADE)
+    character_id = models.ForeignKey(Character, on_delete=models.CASCADE)
+    npc_id = models.ForeignKey(Npc, on_delete=models.CASCADE)
+    cast_id = models.ForeignKey(Cast, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     image = models.CharField(max_length=300)
     date_created = models.DateField()
--- warlockapi/models/character.py
+++ warlockapi/models/character.py
@@ -1,10 +1,10 @@
 from django.db import models
-# from .user import User
+from .user import User
 from .campaign import Campaign
 
 class Character(models.Model):
 
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     image = models.CharField(max_length=300)
